16e4bee4ec6320a41c7f019774550e79940fce1e
Miguel Casas <miguel@kommit.co>
2015-12-19 01:24:33 -0500

    cual es mi navegador

--- solution.rb
+++ solution.rb
@@ -1,19 +1,5 @@
 require "sinatra"
 
-enable :sessions
-
 get '/' do
-  erb:form
-end
-
-post '/response' do
-  if params[:data].match(/[^A-Z]/) 
-    <<-HTML
-     <h1>habla mas durito mijo</h1>
-     HTML
-  else
-    <<-HTML
-     <h1>Ahh si , manzanas</h1>
-     HTML
-  end
+request.env['HTTP_USER_AGENT']
 end
